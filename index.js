a1c2630af7d76e5b7dbdc8b3f2dc55d50247df0a
Tristan Schmitt <trschmitt50@live.com>
2017-07-26 15:45:04 -0400

    Committing changes that include:   Beginning of the html file to layout the form elements and list elements     Added form element, submit button and input button to work with the search/text bar   Began the CSS file

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 const express = require('express');
 const app = express();
+const mustacheExpress = require('mustache-express');
+const bodyParser = require("body-parser");
+const expressValidator = require("express-validator");
+
+//setup body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//setup express validator
+app.use(expressValidator());
+
+//setup router and connect other JS filess
+app.use(require("./studentRoutes"))
+
+//register '.mustache'
+app.engine("mustache", mustacheExpress());
+
+//turn on default template engine
+app.set("view engine", "mustache");
+
+//stored views
+app.set("views", __dirname + "/views");
+
+app.listen(3010, () => {
+  console.log("Successfuly running in http://localhost:3010");
+})
